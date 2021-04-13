aac24c5a068af4fa7840970a3c04d4029fc1a6ab
Thananon Pongsuwan <thenightraven7407@gmail.com>
2021-04-13 11:08:29 +0300

    Link UserRegister.js

--- src/pages/User/UserRegister.js
+++ src/pages/User/UserRegister.js
@@ -3,20 +3,64 @@ import {useField} from "../../hooks";
 import {useHistory} from "react-router";
 import {loginWithoutCredential, logoutWithoutCredential} from "../../reducers/loginReducer";
 import {connect} from "react-redux";
+import {gql} from "@apollo/client/core";
+import {useMutation} from "@apollo/client";
 
-const UserRegister = (props ) => {
+const ADD_USER = gql`
+    mutation AddUser(
+        $email:String,
+        $firstName:String,
+        $lastName:String,
+        $password:String
+    ){
+        addUser(
+            email: $email,
+            firstName: $firstName,
+            lastName: $lastName,
+            password: $password
+        ) {
+            email
+            firstName
+        }
+    }
+
+`
+
+const UserRegister = (props) => {
     const history = useHistory()
     const email = useField('email')
     const firstName = useField('text')
     const lastName = useField('text')
     const password = useField('password')
     const confirmPassword = useField('password')
+    const [addUser] = useMutation(ADD_USER)
 
     const handleSubmit = (e) => {
         e.preventDefault()
-        alert("New User registered")
-        props.loginWithoutCredential()
-        history.push('/user/home')
+        if (password.value.length < 8 || confirmPassword.value.length < 8) {
+            alert("Password length must be greater than 8 characters")
+            return false
+        } else if (password.value !== confirmPassword.value) {
+            alert("Password does not match!")
+            return false
+        }
+        addUser({
+            variables: {
+                email: email.value,
+                firstName: firstName.value,
+                lastName: lastName.value,
+                password: password.value
+            }
+
+        }).then(result => {
+            alert("New User registered")
+            // props.loginWithoutCredential()
+            history.push('/')
+        }).catch(e => {
+            alert(e)
+            console.log(e)
+        })
+
     }
 
 
@@ -31,45 +75,50 @@ const UserRegister = (props ) => {
         <div>
             <h1>Register New User</h1>
             <Container>
-            <Form onSubmit={handleSubmit} onReset={resetForm}>
-                <Form.Group controlId="formBasicEmail">
-                    <Form.Label>Email address</Form.Label>
-                    <Form.Control type={email.type} placeholder="Enter email" value={email.value}
-                                  onChange={email.onChange}/>
-                </Form.Group>
+                <Form onSubmit={handleSubmit} onReset={resetForm}>
+                    <Form.Group controlId="formBasicEmail">
+                        <Form.Label>Email address (*)</Form.Label>
+                        <Form.Control required type={email.type} placeholder="Enter email" value={email.value}
+                                      onChange={email.onChange}/>
+                    </Form.Group>
 
-                <Form.Group controlId="formBasicFirstName">
-                    <Form.Label>First Name</Form.Label>
-                    <Form.Control type={firstName.type} placeholder="Enter first name" value={firstName.value}
-                                  onChange={firstName.onChange}/>
-                </Form.Group>
+                    <Form.Group controlId="formBasicFirstName">
+                        <Form.Label>First Name (*)</Form.Label>
+                        <Form.Control required type={firstName.type} placeholder="Enter first name"
+                                      value={firstName.value}
+                                      onChange={firstName.onChange}/>
+                    </Form.Group>
 
-                <Form.Group controlId="formBasicSurname">
-                    <Form.Label>Last Name</Form.Label>
-                    <Form.Control type={lastName.type} placeholder="Enter last name" value={lastName.value}
-                                  onChange={lastName.onChange}/>
-                </Form.Group>
+                    <Form.Group controlId="formBasicSurname">
+                        <Form.Label>Last Name (*)</Form.Label>
+                        <Form.Control required type={lastName.type} placeholder="Enter last name" value={lastName.value}
+                                      onChange={lastName.onChange}/>
+                    </Form.Group>
 
-                <Form.Group controlId="formBasicPassword">
-                    <Form.Label>Password</Form.Label>
-                    <Form.Control type={password.type} placeholder="Password" value={password.value}
-                                  onChange={password.onChange}/>
-                </Form.Group>
+                    <Form.Group controlId="formBasicPassword">
+                        <Form.Label>Password (*)</Form.Label>
+                        <Form.Control required type={password.type} placeholder="Password" value={password.value}
+                                      onChange={password.onChange}/>
+                    </Form.Group>
 
-                <Form.Group controlId="formBasicConfirmPassword">
-                    <Form.Label>Confirm Password</Form.Label>
-                    <Form.Control type={confirmPassword.type} placeholder="Password" value={confirmPassword.value}
-                                  onChange={confirmPassword.onChange}/>
-                </Form.Group>
-                <Button type="submit">Submit</Button>
+                    <Form.Group controlId="formBasicConfirmPassword">
+                        <Form.Label>Confirm Password (*)</Form.Label>
+                        <Form.Control required type={confirmPassword.type} placeholder="Password"
+                                      value={confirmPassword.value}
+                                      onChange={confirmPassword.onChange}/>
+                        <Form.Control.Feedback type="invalid">
+                            Please choose a username.
+                        </Form.Control.Feedback>
+                    </Form.Group>
+                    <Button type="submit">Submit</Button>
 
-                <Button variant="secondary" type="reset">
-                    Reset
-                </Button>
-            </Form>
-        </Container>
-</div>
-)
+                    <Button variant="secondary" type="reset">
+                        Reset
+                    </Button>
+                </Form>
+            </Container>
+        </div>
+    )
 }
 
 const mapDispatchToProps = {
