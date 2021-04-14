5aa6e7458f0a893f465ccb43f8f5635d34a4fd30
Thananon Pongsuwan <thenightraven7407@gmail.com>
2021-04-14 16:11:47 +0300

    Show card based on user state

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -14,6 +14,57 @@ const Landing = (props) => {
         history.push('/search')
     }
 
+    const showCard = () => {
+        if (props.login.login) {
+            if (props.login.user.type === "user") {
+                return (
+                    <Card style={{width: '18rem'}}>
+                        <Card.Header as="h5">Welcome {props.login.user.user.firstName}</Card.Header>
+                        <Card.Body>
+                            <Card.Title>Click here to go to your user page.
+                                <Link to="/user/home"><Button>User page</Button></Link>
+                            </Card.Title>
+                        </Card.Body>
+                    </Card>
+                )
+            } else if (props.login.user.type === "staff") {
+                return (<Card style={{width: '18rem'}}>
+                    <Card.Header as="h5">Welcome {props.login.user.user.firstName}</Card.Header>
+                    <Card.Body>
+                        <Card.Title>Click here to go to your staff page
+                            <Link to="/user/home"><Button>Staff page</Button></Link>
+                        </Card.Title>
+                    </Card.Body>
+                </Card>)
+            }
+        } else {
+            return (
+                <CardDeck>
+                    <Card style={{width: '18rem'}}>
+                        <Card.Header as="h5">New user</Card.Header>
+                        <Card.Body>
+                            <Card.Title>Are you new to the library website</Card.Title>
+                            <div>Already has an account
+                                <Link to="/user/login"><Button>Login</Button></Link>
+                            </div>
+                            <div>
+                                No account?
+                                <Link to="/user/register"><Button>Register here</Button></Link>
+                            </div>
+                        </Card.Body>
+                    </Card>
+                    <Card style={{width: '18rem'}}>
+                        <Card.Header as="h5">Staff</Card.Header>
+                        <Card.Body>
+                            <Card.Title>Free candy for staff here</Card.Title>
+                            <Link to="/staff"><Button>Staff login</Button></Link>
+                            <Link to="/staff/register"><Button>Staff Register</Button></Link>
+                        </Card.Body>
+                    </Card>
+                </CardDeck>
+            )
+        }
+    };
     return (
         <Container fluid>
             <h1>Welcome to library system</h1>
@@ -39,62 +90,31 @@ const Landing = (props) => {
                         <ListGroup.Item>Sun<span className="float-right">Closed</span></ListGroup.Item>
                     </ListGroup>
                 </Card>
-                {(props.login.login && props.login.user.type === "user")
-                    ? <Card style={{width: '18rem'}}>
-                        <Card.Header as="h5">Welcome {props.login.user.user.firstName}</Card.Header>
-                        <Card.Body>
-                            <Card.Title>Click here to go to your user page.
-                                <Link to="/user/home"><Button>User page</Button></Link>
-                            </Card.Title>
-                        </Card.Body>
-                    </Card>
-                    : <Card style={{width: '18rem'}}>
-                        <Card.Header as="h5">New user</Card.Header>
-                        <Card.Body>
-                            <Card.Title>Are you new to the library website</Card.Title>
-                            <div>Already has an account
-                                <Link to="/user/login"><Button>Login</Button></Link>
-                            </div>
-                            <div>
-                                No account?
-                                <Link to="/user/register"><Button>Register here</Button></Link>
-                            </div>
-                        </Card.Body>
-                    </Card>
-                }
+                {/*{(props.login.login && props.login.user.type === "user")*/}
+                {/*    ? <Card style={{width: '18rem'}}>*/}
+                {/*        <Card.Header as="h5">Welcome {props.login.user.user.firstName}</Card.Header>*/}
+                {/*        <Card.Body>*/}
+                {/*            <Card.Title>Click here to go to your user page.*/}
+                {/*                <Link to="/user/home"><Button>User page</Button></Link>*/}
+                {/*            </Card.Title>*/}
+                {/*        </Card.Body>*/}
+                {/*    </Card>*/}
+                {/*    : <Card style={{width: '18rem'}}>*/}
+                {/*        <Card.Header as="h5">New user</Card.Header>*/}
+                {/*        <Card.Body>*/}
+                {/*            <Card.Title>Are you new to the library website</Card.Title>*/}
+                {/*            <div>Already has an account*/}
+                {/*                <Link to="/user/login"><Button>Login</Button></Link>*/}
+                {/*            </div>*/}
+                {/*            <div>*/}
+                {/*                No account?*/}
+                {/*                <Link to="/user/register"><Button>Register here</Button></Link>*/}
+                {/*            </div>*/}
+                {/*        </Card.Body>*/}
+                {/*    </Card>*/}
+                {/*}*/}
+                {showCard()}
 
-                {(props.login.login && props.login.user.type === "staff")
-                    ? <Card style={{width: '18rem'}}>
-                        <Card.Header as="h5">Welcome {props.login.user.user.firstName}</Card.Header>
-                        <Card.Body>
-                            <Card.Title>Click here to go to your user page.
-                                <Link to="/user/home"><Button>User page</Button></Link>
-                            </Card.Title>
-                        </Card.Body>
-                    </Card>
-                    : <Card style={{width: '18rem'}}>
-                        <Card.Header as="h5">New user</Card.Header>
-                        <Card.Body>
-                            <Card.Title>Are you new to the library website</Card.Title>
-                            <div>Already has an account
-                                <Link to="/user/login"><Button>Login</Button></Link>
-                            </div>
-                            <div>
-                                No account?
-                                <Link to="/user/register"><Button>Register here</Button></Link>
-                            </div>
-                        </Card.Body>
-                    </Card>
-                }
-
-                <Card style={{width: '18rem'}}>
-                    <Card.Header as="h5">Staff</Card.Header>
-                    <Card.Body>
-                        <Card.Title>Free candy for staff here</Card.Title>
-                        <Link to="/staff"><Button>Staff login</Button></Link>
-                        <Link to="/staff/register"><Button>Staff Register</Button></Link>
-                    </Card.Body>
-                </Card>
 
             </CardDeck>
             {/*<img*/}
