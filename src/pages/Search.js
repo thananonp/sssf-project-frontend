6e40639400aa1b2bfbcb2b93eef7b41ebdba941b
Thananon Pongsuwan <thenightraven7407@gmail.com>
2021-05-07 12:54:46 +0300

    Implement search function Search.js

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -9,10 +9,16 @@ import {ErrorMessage, LoadingSpinner} from "./Components";
 import {useField} from "../hooks";
 
 const SEARCHBOOKS = gql`
-    query SearchBooks($query:String, $scope:String){
+    query SearchBooks($query:String, $scope:String, $limit:Int, $skip:Int){
+        countBookSearch(
+            query:$query,
+            scope:$scope,
+        )
         searchBooks(
             query:$query,
-            scope:$scope
+            scope:$scope,
+            limit:$limit,
+            skip:$skip
         ){
             id
             title
@@ -40,22 +46,42 @@ const SEARCHBOOKS = gql`
 `
 
 const Search = (props) => {
+    const limit = useField(null, 10)
+    let active = useField(null, 1)
     const [search, setSearch] = useState('')
     const {loading, error, data} = useQuery(SEARCHBOOKS, {
         variables: {
             query: props.searchQuery.query ? props.searchQuery.query : '',
-            scope: props.searchQuery.scope
+            scope: props.searchQuery.scope,
+            limit: Number(limit.value),
+            skip: Number(active.value-1)*10
         }
     })
 
-    const numberOfQuery = useField(null,10)
-    let active = useField(null, 1)
+
+    const handleSearch = (e) => {
+        setSearch(e.target.value)
+    }
+
+    const toSearch = () => {
+        props.newSearchQuery(search)
+        setSearch('')
+        active.setValue(1)
+    }
+
+    const setScopeSearch = (e) => {
+        props.searchScope(e.target.value)
+    }
+
+    if (loading) return (<LoadingSpinner/>);
+    if (error) return <ErrorMessage error={error}/>
+
     let items = [];
-    for (let number = 1; number <= 5; number++) {
+    for (let number = 1; number <= (data.countBookSearch/limit.value)+1; number++) {
         if (number === active.value) {
             items.push(
                 <Pagination.Item key={number} active={true}>
-                    WIP:{number}
+                    {number}
                 </Pagination.Item>,
             );
         } else {
@@ -66,22 +92,6 @@ const Search = (props) => {
             );
         }
     }
-
-    const handleSearch = (e) => {
-        setSearch(e.target.value)
-    }
-
-    const toSearch = () => {
-        props.newSearchQuery(search)
-        setSearch('')
-    }
-
-    const setScopeSearch = (e) => {
-        props.searchScope(e.target.value)
-    }
-
-    if (loading) return (<LoadingSpinner/>);
-    if (error) return <ErrorMessage error={error}/>
     return (
         <div>
             <Container>
@@ -112,21 +122,21 @@ const Search = (props) => {
                     </Form.Group>
                 </Form>
 
-                <p>Found {data.searchBooks.length} search results</p>
+                <p>Found {data.countBookSearch} search results</p>
                 <Dropdown className={"float-left"}>
-                    WIP: Show
+                    Show
                     <Dropdown.Toggle variant="outline-primary" id="dropdown-basic">
-                        {numberOfQuery.value}
+                        {limit.value}
                     </Dropdown.Toggle>
                     entries
 
 
                     <Dropdown.Menu>
                         <Dropdown.Header>Select the numbers of entries</Dropdown.Header>
-                        <Dropdown.Item onClick={numberOfQuery.onClick}>10</Dropdown.Item>
-                        <Dropdown.Item onClick={numberOfQuery.onClick}>25</Dropdown.Item>
-                        <Dropdown.Item onClick={numberOfQuery.onClick}>50</Dropdown.Item>
-                        <Dropdown.Item onClick={numberOfQuery.onClick}>100</Dropdown.Item>
+                        <Dropdown.Item onClick={limit.onClick}>10</Dropdown.Item>
+                        <Dropdown.Item onClick={limit.onClick}>25</Dropdown.Item>
+                        <Dropdown.Item onClick={limit.onClick}>50</Dropdown.Item>
+                        <Dropdown.Item onClick={limit.onClick}>100</Dropdown.Item>
                     </Dropdown.Menu>
                 </Dropdown>
                 <Pagination className={"float-right"}>
@@ -152,7 +162,7 @@ const Search = (props) => {
                         // console.log(book)
                         return (
                             <tr>
-                                <td>{index + 1}</td>
+                                <td>{(active.value-1) * 10 + index + 1}</td>
                                 <td><Link to={`/book/${book.id}`}>{book.title}</Link></td>
                                 {book.category !== null ?
                                     <td><Link to={`/category/${book.category.id}`}>{book.category.title}</Link></td> :
